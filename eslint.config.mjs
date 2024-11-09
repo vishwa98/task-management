d6fc59daa54397aa5a432559f057de0343ebdeee
Vishwa Madhawa <dev.vishwamadhawa@gmail.com>
2024-11-09 21:41:11 +0530

    React file structure added with eslint and prettier configurations

---
+++ eslint.config.mjs
@@ -0,0 +1,27 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+import pluginPrettier from "eslint-plugin-prettier";
+import prettierConfig from "eslint-config-prettier";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  prettierConfig,
+  {
+    plugins: { prettier: pluginPrettier },
+    rules: {
+      "prettier/prettier": "warn"
+    },
+    settings: {
+      "import/resolver": {
+        typescript: {}
+      }
+    }
+  }
+];
