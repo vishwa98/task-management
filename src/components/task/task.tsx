c65d8e4a3596126bfede2ad7636cbbf4776b630a
Vishwa Madhawa <dev.vishwamadhawa@gmail.com>
2024-11-17 12:50:58 +0530

    task components added

---
+++ src/components/task/task.tsx
@@ -0,0 +1,26 @@
+import React, { FC, ReactElement } from 'react';
+
+import { Box } from '@mui/material';
+import { TaskHeader } from './_taskHeader';
+
+export const Task: FC = (props): ReactElement => {
+  return (
+    <Box
+      display="flex"
+      width="100%"
+      justifyContent="flex-start"
+      flexDirection="column"
+      mb={2}
+      p={4}
+      sx={{
+        width: '100%',
+        backgroundColor: 'background.paper',
+        borderRadius: '8px',
+        border: '1px solid',
+        borderColor: 'error.light',
+      }}
+    >
+      <TaskHeader />
+    </Box>
+  );
+};
