94991ab0cc16d1c4e8b5e4fc959ccb5413545b35
Vishwa Madhawa <dev.vishwamadhawa@gmail.com>
2025-01-09 19:59:46 +0530

    MySQL integration

--- task-management-api/index.ts
+++ task-management-api/index.ts
@@ -1,9 +1,20 @@
 import express, {Express, Request, Response} from "express";
 
 import dotenv from "dotenv";
+import { DataSource } from 'typeorm'
 
 const app: Express = express();
 dotenv.config();
+
+export const AppDataSource = new DataSource({
+    type: 'mysql',
+    host: 'localhost',
+    port: 3306,
+    username: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+    synchronize: true,
+})
 
 const port = process.env.PORT;
 
@@ -11,4 +22,10 @@ app.get("/", (req: Request, res: Response) => {
     res.send("Task API");
 })
 
-app.listen(port);
+AppDataSource.initialize().then(() => {
+    app.listen(port, () => {
+        console.log(`Server running on port ${port}`);
+    });
+}).catch((error) => {
+    console.log(error);
+});
